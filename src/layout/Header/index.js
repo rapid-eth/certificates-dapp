97cc0bc6835802c0fe99e626d29ae2ac71ed32cd
robbie <robbiekruszynski@gmail.com>
2020-01-08 10:36:55 -0800

    start adjustments in navbar

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import { Link } from "@reach/router"
-import "./index.css"
-//import Balance from "../../components/Balance"
+import React from "react";
+import { Link } from "@reach/router";
+import "./index.css";
+import Button from "@material-ui/core/Button";
 
+//import Balance from "../../components/Balance"
 
 function Header() {
   return (
-
     <div>
       <header>
         <div className="header-div">
-          <Link className="nav-item nav-link" to="/"><button className="btn-primary">Home</button></Link>
-          <Link className="nav-item nav-link" to="/deploy"><button className="btn-secondary">Deploy</button></Link>
-          <Link className="nav-item nav-link" to="/token"><button className="btn-secondary">Token</button></Link>
+          <Link className="nav-item nav-link" to="/">
+            <Button varient="contained" color="secondary">
+              Home
+            </Button>
+          </Link>
+          <Link className="nav-item nav-link" to="/deploy">
+            <Button varient="contained" color="secondary">
+              Deployed
+            </Button>
+          </Link>
+          <Link className="nav-item nav-link" to="/token">
+            <Button varient="contained" color="secondary">
+              Token
+            </Button>
+          </Link>
         </div>
         {/* <div className="balance-tab">
             <Balance />
           </div> */}
       </header>
     </div>
-
   );
 }
 
