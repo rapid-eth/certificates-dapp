0b60d7247c645f8c2213101e3e11325860ab9bb9
robbie <robbiekruszynski@gmail.com>
2020-01-08 10:47:45 -0800

    update navbar with menu + animation import

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -2,29 +2,120 @@ import React from "react";
 import { Link } from "@reach/router";
 import "./index.css";
 import Button from "@material-ui/core/Button";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 //import Balance from "../../components/Balance"
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  paper: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
 function Header() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = event => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
     <div>
       <header>
         <div className="header-div">
-          <Link className="nav-item nav-link" to="/">
-            <Button varient="contained" color="secondary">
-              Home
-            </Button>
-          </Link>
-          <Link className="nav-item nav-link" to="/deploy">
-            <Button varient="contained" color="secondary">
-              Deployed
-            </Button>
-          </Link>
-          <Link className="nav-item nav-link" to="/token">
-            <Button varient="contained" color="secondary">
-              Token
-            </Button>
-          </Link>
+          <Button
+            ref={anchorRef}
+            aria-controls={open ? "menu-list-grow" : undefined}
+            aria-haspopup="true"
+            onClick={handleToggle}
+          >
+            Menu
+          </Button>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin:
+                    placement === "bottom" ? "center top" : "center bottom"
+                }}
+              >
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList
+                      autoFocusItem={open}
+                      id="menu-list-grow"
+                      onKeyDown={handleListKeyDown}
+                    >
+                      <MenuItem onClick={handleClose}>
+                        <Link className="nav-item nav-link" to="/">
+                          <Button varient="contained" color="secondary">
+                            Home
+                          </Button>
+                        </Link>
+                      </MenuItem>
+                      <MenuItem onClick={handleClose}>
+                        {" "}
+                        <Link className="nav-item nav-link" to="/deploy">
+                          <Button varient="contained" color="secondary">
+                            Deployed
+                          </Button>
+                        </Link>
+                      </MenuItem>
+                      <MenuItem onClick={handleClose}>
+                        {" "}
+                        <Link className="nav-item nav-link" to="/token">
+                          <Button varient="contained" color="secondary">
+                            Token
+                          </Button>
+                        </Link>
+                      </MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
         </div>
         {/* <div className="balance-tab">
             <Balance />
