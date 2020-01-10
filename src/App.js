0edffd63a3b94810d25cc85660b51a8cd71d29ec
robbie <robbiekruszynski@gmail.com>
2020-01-09 18:14:28 -0800

    add hash routing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Body from "./layout/Body";
 import Footer from "./layout/Footer";
 import "./App.css";
 import { createHistory, LocationProvider, Router } from "@reach/router";
-import createHashSource from "hash-source";
 import Home from "./pages/Home";
 import TokenList from "./pages/TokenList";
 import Deploy from "./pages/Deploy";
 import TokenPage from "./pages/Token";
+import createHashSource from "hash-source";
+
 import { MyWeb3Provider } from "./web3/EthersContext";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./dAppTheme";
@@ -17,22 +18,24 @@ let history = createHistory(source);
 
 function App() {
   return (
-    <div className="app-class">
-      <MyWeb3Provider>
-        <MuiThemeProvider theme={theme}>
-          <Header />
-          <Body />
-          <Router>
-            <Home path="/" />
-            <TokenList path="/list" />
-            <Deploy path="/deploy" />
-            <TokenPage path="/token" />
-            <TokenPage path="/token/:tokenId" />
-          </Router>
-        </MuiThemeProvider>
-      </MyWeb3Provider>
-      <MuiThemeProvider theme={theme}></MuiThemeProvider>
-    </div>
+    <LocationProvider>
+      <div className="app-class">
+        <MyWeb3Provider>
+          <MuiThemeProvider theme={theme}>
+            <Header />
+            <Body />
+            <Router>
+              <Home path="/" />
+              <TokenList path="/list" />
+              <Deploy path="/deploy" />
+              <TokenPage path="/token" />
+              <TokenPage path="/token/:tokenId" />
+            </Router>
+          </MuiThemeProvider>
+        </MyWeb3Provider>
+        <MuiThemeProvider theme={theme}></MuiThemeProvider>
+      </div>
+    </LocationProvider>
   );
 }
 
